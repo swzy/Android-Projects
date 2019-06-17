504dddc3446d392dae21da129e6195c6dbe0c250
Sam Yi <swzy92@gmail.com>
2019-06-16 23:16:52 -0400

    Latest update

---
+++ Assignments/Assignment1/app/src/main/java/com/yisam/assignment1/MainActivity.java
@@ -0,0 +1,19 @@
+package com.yisam.assignment1;
+
+import android.databinding.DataBindingUtil;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.yisam.assignment1.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        User user = new User("Samuel Yi", "***REMOVED***");
+        binding.setUser(user);
+    }
+}
